74a91803311a300d48ea6ed89ad22f605d324729
igorp-lopes <igorp.lopes16@gmail.com>
2021-04-14 12:41:49 -0300

    Criado programa de teste para classe Animacao

--- lab03/src/mc322/lab03/AppLab03.java
+++ lab03/src/mc322/lab03/AppLab03.java
@@ -3,16 +3,14 @@ package mc322.lab03;
 public class AppLab03 {
 
 	public static void main(String[] args) {
-		
-		AquarioLombriga lombriga1 = new AquarioLombriga(8, 4, 3);
-		
-		for (int i = 1; i < 10; i++) {
-			System.out.println(lombriga1.apresenta());
-			if (i % 2 == 0) {
-				lombriga1.crescer();
-			}
-			lombriga1.mover();
-			System.out.println();
+
+		Animacao anim1 = new Animacao("080403MCMVM");
+		String output;
+
+		for (int i = 0; i < 6; i++) {
+			output = anim1.apresenta();
+			System.out.println(output);
+			anim1.passo();
 		}
 	}
 
